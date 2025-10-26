846e48e54d045338da1aeb032abd3c73ca54b824
SENX | センクス <senseixdev@gmail.com>
2025-10-26 23:16:29 +0000

    sm8250-common: kernel: Update flags for clang 20+
    * Fixes an issue where the kernel build would fail when
    compiled with Clang 20+ due to the target triple format.
    
    * Removed the incompatible version suffix or adjusted the
    triple format to ensure successful compilation with modern
    LLVM/Clang toolchains.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,8 +105,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := r574158
-KERNEL_CC := CC=$(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)/bin/clang
+KERNEL_CC := CC=$(shell pwd)/prebuilts/clang/host/linux-x86/clang-r574158/bin/clang
 KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 LLVM_IAS=1
@@ -117,6 +116,10 @@ HWUI_COMPILE_FOR_PERF := true
 
 # Media
 TARGET_USES_ION := true
+
+# Kernel Clang Flags
+KERNEL_CC := CC=clang
+override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
